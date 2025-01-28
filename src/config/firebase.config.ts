499b23d22be69061bc4687d6cb9dfe0ce0aea1f1
camarab <bafode.camara@sacem.fr>
2025-01-28 21:10:06 +0100

    init firebase service account

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,33 +1,6 @@
 import * as admin from 'firebase-admin';
 import { ServiceAccount } from 'firebase-admin';
-import { config } from '.';
-
-interface ExtendedServiceAccount extends ServiceAccount {
-    type?: string;
-    privateKeyId?: string;
-    clientId?: string;
-    authUri?: string;
-    tokenUri?: string;
-    authProviderX509CertUrl?: string;
-    clientC509CertUrl?: string;
-    universeDomain?: string;
-}
-
-
-const serviceAccount: ExtendedServiceAccount = {
-    type: config.firebase.type,
-    projectId: config.firebase.projectId,
-    clientEmail: config.firebase.clientEmail,
-    privateKey: config.firebase.privateKey?.replace(/\\n/g, '\n'),
-    privateKeyId: config.firebase.privateKeyId,
-    clientId: config.firebase.clientId,
-    authUri: config.firebase.authUri,
-    tokenUri: config.firebase.tokenUri,
-    authProviderX509CertUrl: config.firebase.authProviderCertUrl,
-    clientC509CertUrl: config.firebase.clientCertUrl,
-    universeDomain: config.firebase.universeDomain,
-};
-
+import serviceAccount from "./service-account.json";
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as ServiceAccount),
